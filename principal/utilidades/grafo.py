f60e9556ef1056d2f64b810b9f6f491e4acc5ca3
talisson <144066825+talissonmendes@users.noreply.github.com>
2024-02-18 20:49:56 -0300

    atualizações no grafo na função bellman-ford para imprimir o trajeto.py

--- principal/utilidades/grafo.py
+++ principal/utilidades/grafo.py
@@ -21,27 +21,37 @@ class Grafo():
 
   # função para a realização do algortimo bellman-ford
   def bellman_ford(self, origem, destino):
-        distancias = {v: float('inf') for v in self.vertices}
-        predecessores = {v: None for v in self.vertices}
-        distancias[origem] = 0
+      distancias = {v: float('inf') for v in self.vertices}
+      predecessores = {v: None for v in self.vertices}
+      distancias[origem] = 0
 
-        for _ in range(len(self.vertices) - 1):
-            for nome_a, vertice in self.vertices.items():
-                for nome_b, distancia in vertice.vizinhos.items():
-                    if distancias[nome_a] + distancia < distancias[nome_b]:
-                        distancias[nome_b] = distancias[nome_a] + distancia
-                        predecessores[nome_b] = nome_a
+      for _ in range(len(self.vertices) - 1):
+          for nome_a, vertice in self.vertices.items():
+              for nome_b, distancia in vertice.vizinhos.items():
+                  if distancias[nome_a] + distancia < distancias[nome_b]:
+                      distancias[nome_b] = distancias[nome_a] + distancia
+                      predecessores[nome_b] = nome_a
 
-        # aqui verifica se há ciclos de peso negativo
-        for nome_a, vertice in self.vertices.items():
-            for nome_b, distancia in vertice.vizinhos.items():
-                if distancias[nome_a] + distancia < distancias[nome_b]:
-                    raise ValueError("O grafo contém um ciclo de peso negativo.")
+      # aqui verifica se há ciclos de peso negativo
+      for nome_a, vertice in self.vertices.items():
+          for nome_b, distancia in vertice.vizinhos.items():
+              if distancias[nome_a] + distancia < distancias[nome_b]:
+                  raise ValueError("O grafo contém um ciclo de peso negativo.")
 
-        # agora imprime o caminho mínimo (opcional)
-        self.imprimir_caminho_minimo(origem, destino, predecessores)
+      # agora imprime o caminho mínimo (opcional)
+      caminho = self.construir_caminho(origem, destino, predecessores)
 
-        return distancias[destino]
+      return distancias[destino], caminho
+
+  # gera a lista de estações do trajeto entre a estação de origem e a estação de destino
+  def construir_caminho(self, origem, destino, predecessores):
+      caminho = [destino]
+      anterior = predecessores[destino]
+      while anterior:
+          caminho.insert(0, anterior)
+          anterior = predecessores[anterior]
+
+      return caminho
 
   def imprimir_caminho_minimo(self, origem, destino, predecessores):
       caminho = [destino]
