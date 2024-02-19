8bbaa397606f16b6dc48e33a5dbe98b68a72777d
Luiz Miguel Veloso <luizzmiguel17@gmail.com>
2024-02-18 21:46:13 -0300

    interface

--- principal/interface.py
+++ principal/interface.py
@@ -2,35 +2,35 @@ import pygame
 import sys
 from main import metro
 
-# Inicialize o Pygame
+origem = ""
+destino = ""
+resolveu = False
+
+# Inicializando o Pygame
 pygame.init()
 
-# Defina as dimensões da janela
+# Definindo as dimensões da janela
 WIDTH, HEIGHT = 850, 650
 WINDOW_SIZE = (WIDTH, HEIGHT)
 
-# Defina as cores
+# Definindo as cores
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
-# Defina as coordenadas dos círculos como uma lista de tuplas (x, y)
+# Definindo as coordenadas dos círculos como uma lista de tuplas (x, y)
 circle_positions = [(100, 100), (200, 200), (300, 300)]
 
 def escrever(texto, tamanho, coordenada, cor):
-  text_surface = pygame.font.Font(None, int(2*zoom_factor)).render(estacao.nome, True, BLACK)
-  text_rect = text_surface.get_rect(topleft=(x-10*zoom_factor, y - 3*zoom_factor))  # Posição do texto na tela
+  text_surface = pygame.font.Font(None, int(tamanho)).render(texto, True, cor)
+  text_rect = text_surface.get_rect(topleft=(coordenada))  # Posição do texto na tela
   screen.blit(text_surface, text_rect) 
 
-# Crie a janela
+# Criando a janela
 screen = pygame.display.set_mode(WINDOW_SIZE)
 pygame.display.set_caption("Círculos em Coordenadas")
-
-estacoes = [estacao.coordenada for estacao in metro.vertices.values()]
-
-print(*estacoes, sep="\n")
 
 zoom_factor = 1
 move_hori = 0
@@ -48,7 +48,7 @@ pygame.key.set_repeat(300, 30)
 
 # Loop principal
 while True:
-  # Verifique eventos
+  # Verificando eventos
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       pygame.quit()
@@ -80,16 +80,28 @@ while True:
       # mover baixo
       elif event.key == pygame.K_DOWN:
         move_vert -= movimento
+
+      if origem == "" or destino == "":
+        if event.key == pygame.K_RETURN:
+          print("Texto inserido:", input_text)  # Apenas para demonstração, você pode modificar conforme necessário
+          
+          if origem == "":
+            origem = input_text
+          
+          elif destino == "":
+            destino = input_text
+            
+          input_text = ''  # Limpa o texto após pressionar Enter
+        elif event.key == pygame.K_BACKSPACE:
+          input_text = input_text[:-1]  # Remove o último caractere
+        else:
+          input_text += event.unicode  # Adiciona o caractere digitado à variável de texto
+
       # Limita o zoom mínimo
       zoom_factor = max(0.1, zoom_factor)
 
-  # Preencha o fundo com a cor branca
+  # Preenchendo o fundo com a cor branca
   screen.fill(WHITE)
-  
-  # Obtenha a posição atual do mouse
-  # mouse_pos = pygame.mouse.get_pos()
-  
-  # move_hori, move_vert = mouse_pos
   
   for estacao in metro.vertices.values():
     loc = estacao.coordenada
@@ -100,7 +112,7 @@ while True:
     coord = (x, y)
     pygame.draw.circle(screen, BLACK, coord, 5)
 
-    escrever(estacao.nome, 2*zoom_factor, ((x-10)*zoom_factor, (y-3)*zoom_factor), BLACK)
+    escrever(estacao.nome, 2*zoom_factor, (x+10, y-3), BLACK)
   
   for linha in metro.conexoes:
     xa = linha[0][0]*zoom_factor + move_hori
@@ -113,9 +125,23 @@ while True:
 
     pygame.draw.line(screen, BLACK, (xa,ya), (xb,yb), espessura)
 
-    text_surface = pygame.font.Font(None, int(10)).render("Pg Up - zoom in", True, BLACK)
-    text_rect = text_surface.get_rect(topleft=(10,10))  # Posição do texto na tela
-    screen.blit(text_surface, text_rect) 
+  pygame.draw.rect(screen, (230,230,230),(0,00,150+ max(len(input_text),len(origem), len(destino))*25,90))
 
-  # Atualize a tela
+  if origem == "":
+    escrever("Origem: "+input_text, 50, (10,10), BLACK)
+    escrever("Destino: ", 50, (10,55), BLACK)
+  
+  elif destino == "":
+    escrever("Origem: "+origem+" +", 50, (10,10), BLACK)
+    escrever("Destino: "+input_text, 50, (10,55), BLACK)
+  
+  else:
+    escrever("Origem: "+origem+" +", 50, (10,10), BLACK)
+    escrever("Destino: "+destino+" +", 50, (10,55), BLACK)
+
+    
+    
+  # escrever("Estação de origem: "+input_text, )  
+
+  # Atualizar a tela
   pygame.display.flip()
